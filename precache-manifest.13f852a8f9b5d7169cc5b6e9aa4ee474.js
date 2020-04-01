3ce276c1968f4ed18f0b532d72cdda243157463d
Andres Long <andreslong95@gmail.com>
2020-04-01 17:20:27 -0500

    Updates

---
+++ precache-manifest.13f852a8f9b5d7169cc5b6e9aa4ee474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827a58970476cbbb83dc6634cb201c77",
+    "url": "/Employee/index.html"
+  },
+  {
+    "revision": "775bb94aaa866ec476a2",
+    "url": "/Employee/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "317a14c0daa8811105f6",
+    "url": "/Employee/static/js/2.7b6bb85e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee/static/js/2.7b6bb85e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775bb94aaa866ec476a2",
+    "url": "/Employee/static/js/main.35371f30.chunk.js"
+  },
+  {
+    "revision": "08195baf324550d74105",
+    "url": "/Employee/static/js/runtime-main.037f0256.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Employee/static/media/logo.5d5d9eef.svg"
+  }
+]);
